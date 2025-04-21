aa73072f1bac189b8df96c5f1a668781e0ac3194
Webs <gamerbryant69@gmail.com>
2025-04-22 06:44:51 +0700

    Main Menu (Button blm pixel) + Camera Smooth
    babi

--- new-game-project/main_menu.gd
+++ new-game-project/main_menu.gd
@@ -13,3 +13,15 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_load_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
