fe338b91d2baacf62f725c6d9d5b68ee30784545
Webs <gamerbryant69@gmail.com>
2025-05-06 09:11:34 +0700

    background + musik

---
+++ new-game-project/scenes/parallax_layer_2.gd
@@ -0,0 +1,9 @@
+extends ParallaxLayer
+
+@export var CLOUD_SPEED = -15
+
+func _process(delta: float) -> void:
+	self.motion_offset.x += CLOUD_SPEED * delta
+
+func _ready() -> void:
+	pass
--- new-game-project/scenes/player.gd
+++ new-game-project/scenes/player.gd
@@ -3,19 +3,18 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var attack_sound: AudioStreamPlayer = $AudioStreamPlayer  # Reference to AudioStreamPlayer node
+@onready var attack_sound: AudioStreamPlayer = $AudioStreamPlayer
 
 var is_attacking = false
 var is_guarding = false
 var current_attack_index = 0
-
+var was_running = false
 var attack_animations = ["attack", "attack1", "attack2"]
 
 func _ready():
 	randomize()
 
 func _physics_process(delta: float) -> void:
-	# Handling Guard
 	if Input.is_action_pressed("guard") and not is_attacking:
 		if not is_guarding:
 			is_guarding = true
@@ -29,51 +28,60 @@ func _physics_process(delta: float) -> void:
 	elif is_guarding:
 		is_guarding = false
 
-	# Handling Attack
 	if Input.is_action_just_pressed("attack") and not is_attacking and not is_guarding:
-		var current_attack_animation = attack_animations[current_attack_index]
-		animated_sprite_2d.animation = current_attack_animation
-		animated_sprite_2d.frame = 0
-		is_attacking = true
-		velocity.x = 0
+		if abs(velocity.x) > 0.1 and is_on_floor():
+			# Running attack
+			animated_sprite_2d.animation = "runattack"
+			animated_sprite_2d.frame = 0
+			is_attacking = true
+			was_running = true
+		else:
+			# Normal combo attack
+			var current_attack_animation = attack_animations[current_attack_index]
+			animated_sprite_2d.animation = current_attack_animation
+			animated_sprite_2d.frame = 0
+			is_attacking = true
+			was_running = false
 
-		# Play attack sound effect
-		attack_sound.play()  # Plays the attack sound
-
+		attack_sound.play()
 		return
 
-	# If attacking, manage attack animation and movement
 	if is_attacking:
-		var current_attack_animation = attack_animations[current_attack_index]
-		if animated_sprite_2d.animation == current_attack_animation and animated_sprite_2d.frame == animated_sprite_2d.sprite_frames.get_frame_count(current_attack_animation) - 1:
+		var current_anim = animated_sprite_2d.animation
+		var is_last_frame = animated_sprite_2d.frame == animated_sprite_2d.sprite_frames.get_frame_count(current_anim) - 1
+
+		if is_last_frame:
 			is_attacking = false
-			current_attack_index = (current_attack_index + 1) % attack_animations.size()
+			if current_anim == "runattack":
+				# Stop momentum after runattack and reset combo
+				velocity.x = 0
+				current_attack_index = 0
+			else:
+				current_attack_index = (current_attack_index + 1) % attack_animations.size()
 		else:
-			velocity.x = 0
+			# Maintain momentum only for runattack
+			if current_anim != "runattack":
+				velocity.x = 0
 			if not is_on_floor():
 				velocity += get_gravity() * delta
 			move_and_slide()
 			return
 
-	# Handling Jump
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Handling Movement
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
-		animated_sprite_2d.flip_h = direction < 0  # Flip character when moving left
+		animated_sprite_2d.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, 30)
 
-	# Move the character
 	move_and_slide()
 
-	# Update animations based on state
 	if not is_attacking and not is_guarding:
 		if not is_on_floor():
 			animated_sprite_2d.animation = "jump"
