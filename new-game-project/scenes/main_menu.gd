0e0397c28e75f75b6a0675557c67a1b704c1c250
Reynaldy <reynaldygamer@gmail.com>
2025-05-12 03:48:31 +0700

    popup besar :v

--- new-game-project/scenes/main_menu.gd
+++ new-game-project/scenes/main_menu.gd
@@ -19,18 +19,19 @@ func _on_load_pressed() -> void:
 	pass # Replace with function body.
 
 
-func _on_options_pressed():
-	$OptionsPopup.popup_centered()
+func _on_options_pressed() -> void:
+	var screen_size = get_viewport().get_visible_rect().size
+	$OptionsPopup.popup_centered(Vector2(screen_size.x * 0.9, screen_size.y * 0.9)) # 90% dari layar
 
-func _on_options_popup_id_pressed(id):
-	match id:
-		0:
-			print("Graphics Settings selected")
-		1:
-			print("Audio Settings selected")
-		2:
-			print("Controls selected")
-			
+
+func _on_CheckBoxNoSound_toggled(button_pressed: bool) -> void:
+	if button_pressed:
+		print("No Sound enabled")
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	else:
+		print("No Sound disabled")
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
+
 func _on_exit_pressed() -> void:
 	#$VBoxContainer/Button4/PopupExit.popup_centered()
 	get_tree().quit()
