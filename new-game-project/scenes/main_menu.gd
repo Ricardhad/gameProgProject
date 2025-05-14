97831edc2177ba763a67c09bee62c372a56b6891
Reynaldy <reynaldygamer@gmail.com>
2025-05-15 04:17:25 +0700

    UI dan Fungsi Option PopUp jalan

--- new-game-project/scenes/main_menu.gd
+++ new-game-project/scenes/main_menu.gd
@@ -20,17 +20,20 @@ func _on_load_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	var screen_size = get_viewport().get_visible_rect().size
-	$OptionsPopup.popup_centered(Vector2(screen_size.x * 0.9, screen_size.y * 0.9)) # 90% dari layar
+	$Panel2.visible = true
+
+func _on_ButtonBack_pressed() -> void:
+	print("Back button pressed")
+	$Panel2.visible = false
 
 
-func _on_CheckBoxNoSound_toggled(button_pressed: bool) -> void:
+func _on_CheckButton_toggled(button_pressed: bool) -> void:
+	print("CheckButton toggled:", button_pressed)
 	if button_pressed:
-		print("No Sound enabled")
-		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+		$AudioStreamPlayer.stream_paused = true
 	else:
-		print("No Sound disabled")
-		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
+		$AudioStreamPlayer.stream_paused = false
+
 
 func _on_exit_pressed() -> void:
 	#$VBoxContainer/Button4/PopupExit.popup_centered()
