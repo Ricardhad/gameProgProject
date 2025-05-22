625a48ac4ade4d2068d73496237ff4461032a548
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2025-05-22 20:58:15 +0700

    rey button e bok kasih nama pentil

--- new-game-project/scenes/ui/selecting_chr.gd
+++ new-game-project/scenes/ui/selecting_chr.gd
@@ -12,3 +12,6 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_backbtn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
