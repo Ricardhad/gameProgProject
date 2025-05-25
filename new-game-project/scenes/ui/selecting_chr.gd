888e1fe64a3b8d2efbc1828ce8ae87592cb95476
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2025-05-25 17:22:49 +0700

    dongo

--- new-game-project/scenes/ui/selecting_chr.gd
+++ new-game-project/scenes/ui/selecting_chr.gd
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/map/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/map/tutorial.tscn")
 
 func _on_backbtn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
