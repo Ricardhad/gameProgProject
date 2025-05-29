c95fa1ae717f088ad7a893363cc0354c713aeedd
Reynaldy <reynaldygamer@gmail.com>
2025-05-30 03:47:08 +0700

    yey animasi transition jadi

--- new-game-project/scenes/ui/main_menu.gd
+++ new-game-project/scenes/ui/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var fade_rect = $VBoxContainer/Button/ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Bgm.play_music_level()
@@ -11,6 +11,23 @@ func _ready() -> void:
 	slider.max_value = 1.0
 	slider.step = 0.01
 	slider.value = db_to_linear(Bgm.volume_db)
+	
+	fade_rect.visible = false
+	fade_rect.color = Color.BLACK  # Pastikan warna hitam
+	fade_rect.modulate.a = 0.0  # Transparan dari awal
+	
+	fade_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	if GlobalVar.transition_fade_in:
+		GlobalVar.transition_fade_in = false  # Reset flag
+
+		fade_rect.visible = true
+		fade_rect.modulate.a = 1.0  # Mulai dari hitam
+
+		var fade_in = create_tween()
+		fade_in.tween_property(fade_rect, "modulate:a", 0.0, 1.0)  # Fade-out dari hitam
+	else:
+		fade_rect.visible = false  # Jika bukan transisi
 	pass # Replace with function body.
 
 
@@ -20,6 +37,18 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
+	print("Button start pressed")
+
+	GlobalVar.transition_fade_in = true  # Aktifkan flag untuk fade-in di scene selanjutnya
+
+	fade_rect.visible = true
+	fade_rect.modulate.a = 0.0
+
+	var fade_out = create_tween()
+	fade_out.tween_property(fade_rect, "modulate:a", 1.0, 1.0)
+	fade_out.tween_callback(Callable(self, "_change_scene"))
+
+func _change_scene() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/selecting_chr.tscn")
 
 
--- new-game-project/scenes/ui/selecting_chr.gd
+++ new-game-project/scenes/ui/selecting_chr.gd
@@ -1,8 +1,20 @@
 extends Node
-
+@onready var fade_rect = $ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	fade_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+
+	if GlobalVar.transition_fade_in:
+		GlobalVar.transition_fade_in = false  # Reset flag
+
+		fade_rect.visible = true
+		fade_rect.modulate.a = 1.0  # Mulai dari hitam
+
+		var fade_in = create_tween()
+		fade_in.tween_property(fade_rect, "modulate:a", 0.0, 1.0)  # Fade-out dari hitam
+	else:
+		fade_rect.visible = false  # Jika bukan transisi
 	pass # Replace with function body.
 
 
@@ -14,4 +26,14 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/map/tutorial.tscn")
 
 func _on_backbtn_pressed() -> void:
+	GlobalVar.transition_fade_in = true  # Aktifkan flag untuk fade-in di scene selanjutnya
+
+	fade_rect.visible = true
+	fade_rect.modulate.a = 0.0
+
+	var fade_out = create_tween()
+	fade_out.tween_property(fade_rect, "modulate:a", 1.0, 1.0)
+	fade_out.tween_callback(Callable(self, "_change_scene1"))
+
+func _change_scene1() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
