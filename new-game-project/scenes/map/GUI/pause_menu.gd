bd17b4d6a81486e99b13321a6b0730f233099445
Webs <gamerbryant69@gmail.com>
2025-06-08 18:15:10 +0700

    volume setting udh fix, sisa full screen blm save setiap kali auto run

--- new-game-project/scenes/map/GUI/pause_menu.gd
+++ new-game-project/scenes/map/GUI/pause_menu.gd
@@ -1,26 +1,27 @@
 extends Node2D
 
+@onready var music_slider: HSlider = $CanvasLayer/PanelPause/PanelPauseOption/HSlider
+@onready var sfx_slider: HSlider = $CanvasLayer/PanelPause/PanelPauseOption/HSlider2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var slider = $CanvasLayer/PanelPause/PanelPauseOption/HSlider
-	slider.min_value = 0.0
-	slider.max_value = 1.0
-	slider.step = 0.01
-	slider.value = db_to_linear(Bgm.volume_db)
 	$CanvasLayer/PanelPause.process_mode = Node.PROCESS_MODE_ALWAYS
 	for child in $CanvasLayer/PanelPause.get_children():
 		child.process_mode = Node.PROCESS_MODE_ALWAYS
 	$CanvasLayer/PanelPause.visible = false
 	get_tree().paused = false
+	
+	var music_db = AudioServer.get_bus_volume_db(1)
+	music_slider.value = db_to_linear(music_db)
+
+	var sfx_db = AudioServer.get_bus_volume_db(2)
+	sfx_slider.value = db_to_linear(sfx_db)
 
 func _on_HSliderMusic_value_changed_tutotrial1(value: float) -> void:
-	var db = linear_to_db(value)
-	Bgm.volume_db = db
-	#print("Volume set to:", db, "dB")
+	AudioServer.set_bus_volume_db(1,linear_to_db(value))
 
 func _on_HSliderMusic_value_changed_tutotrial2(value: float) -> void:
-	AudioServer.set_bus_volume_db(2, linear_to_db($CanvasLayer/PanelPause/PanelPauseOption/HSlider2.value))
-	#print("Volume set to:", db, "dB")
+	AudioServer.set_bus_volume_db(2,linear_to_db(value))
 
 func _on_CheckButton2_toggled1(button_pressed: bool) -> void:
 	if button_pressed:
@@ -31,8 +32,6 @@ func _on_CheckButton2_toggled1(button_pressed: bool) -> void:
 func _on_options_pressed1() -> void:
 	var option_panel = $CanvasLayer/PanelPause/PanelPauseOption
 	option_panel.visible = not option_panel.visible
-
-
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -47,9 +46,8 @@ func _on_Resumebtn_pressed() -> void:
 	print("Resume pressed")
 	$CanvasLayer/PanelPause.visible = false
 	get_tree().paused = false
+	$CanvasLayer/PanelPause/PanelPauseOption.visible = false
 	
-
-
 func _on_Exitbtn_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
