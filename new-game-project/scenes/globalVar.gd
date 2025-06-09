a481178da8dfafa34eb1e4cd076c329026a159eb
Webs <gamerbryant69@gmail.com>
2025-06-10 01:09:14 +0700

    hp item bisa nambah darah

--- new-game-project/scenes/globalVar.gd
+++ new-game-project/scenes/globalVar.gd
@@ -8,6 +8,12 @@ var score = 0
 var kill_count = 0
 var current_stage = "1-1"
 
+var hp = 0
+var atk = 0
+var def = 0
+var agi = 0
+var maxPot = 0
+
 func add_coin():
 	coin_collected += 1
 	print(coin_collected)
@@ -20,3 +26,24 @@ func reset_game():
 	score = 0
 	kill_count = 0
 	current_stage = "1-1"
+	
+	hp = 0
+	atk = 0
+	def = 0
+	agi = 0
+	maxPot = 0
+
+func add_item(itemName: String):
+	if(itemName == "hp"):
+		hp += 1
+		maxhealth_player += 3
+		health_player += 3
+	elif(itemName == "atk"):
+		atk += 1
+		damage_player += 2
+	elif(itemName == "def"):
+		def += 1
+	elif(itemName == "agi"):
+		agi += 1
+	elif(itemName == "maxPot"):
+		maxPot += 1
