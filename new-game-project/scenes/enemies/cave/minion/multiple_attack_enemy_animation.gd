f102588c4c940dc121b2213d7383fcf4cbeccf25
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2025-06-10 03:47:23 +0700

    dongo push minion part 4

---
+++ new-game-project/scenes/enemies/cave/minion/multiple_attack_enemy_animation.gd
@@ -0,0 +1,47 @@
+extends "res://scenes/enemies/component/AnimationController.gd"
+
+
+func _ready() -> void:
+	state_machine.state_changed.connect(_on_state_changed)
+	health_component.health_depleted.connect(_on_death)
+	self.animation_finished.connect(_on_animation_finished)
+
+
+# This function now correctly accepts BOTH arguments from the signal.
+func _on_state_changed(new_state: StateMachine.State, previous_state: StateMachine.State) -> void:
+	# If the new state isn't HURT, we can update our "last_state" memory.
+	# This ensures we don't forget we were chasing if we get hurt.
+	if new_state != StateMachine.State.HURT:
+		last_state = new_state
+	
+	match new_state:
+		StateMachine.State.IDLE:
+			play("idle")
+		StateMachine.State.PATROL:
+			play("walk")
+		StateMachine.State.CHASE:
+			play("run")
+		StateMachine.State.ATTACK:
+			play("attack1")
+		StateMachine.State.HURT:
+			play("hurt")
+		StateMachine.State.DEAD:
+			play("dead")
+
+func _on_death() -> void:
+	# We can just call our state change function directly.
+	# The StateMachine is the source of truth, so we command IT to change state.
+	state_machine.change_state(StateMachine.State.DEAD)
+
+func _on_animation_finished() -> void:
+	# If the "hurt" animation has just finished...
+	
+	# Inside AnimationController.gd -> _on_animation_finished()
+
+	if animation == "hurt":
+		# This is the new, more aggressive logic
+		state_machine.change_state(StateMachine.State.CHASE)
+	
+	if animation == "dead":
+		# This tells Godot to wait until it's safe before deleting the goblin.
+		get_parent().call_deferred("queue_free")
