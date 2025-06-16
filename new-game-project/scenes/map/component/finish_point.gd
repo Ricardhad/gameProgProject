14ebd79ac7a2f19a333826e657a17a6eabfff06e
Webs <gamerbryant69@gmail.com>
2025-06-16 18:01:05 +0700

    buff gk berkurang pas keluar dari tavern, keybind heavy attack babi

--- new-game-project/scenes/map/component/finish_point.gd
+++ new-game-project/scenes/map/component/finish_point.gd
@@ -16,11 +16,9 @@ func _on_body_entered(body: Node2D):
 		
 		# ▼▼▼ TAMBAHKAN KONDISI INI ▼▼▼
 		# Hanya proses akhir stage jika scene berikutnya BUKAN "OutOfTavern"
-		if next_scene != "OutOfTavern":
-			GlobalVar.process_end_of_stage()
-		else:
-			print("Keluar dari Tavern, buff tidak dihilangkan.")
+		GlobalVar.process_end_of_stage()
 		
 		# Sisa kode untuk pindah scene tetap berjalan
-		GlobalVar.current_stage = next_scene
+		if next_scene != "":
+			GlobalVar.current_stage = next_scene
 		Transition1.change_scene(target_scene.resource_path)
